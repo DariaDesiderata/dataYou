4de7e0e887f5ef076d51ea7d192a3838d5c5071f
DariaDesiderata <daria.calwell@gmail.com>
2017-02-08 16:54:12 -0700

    added style and grace to homepage

---  public/app.js
+++  public/app.js
@@ -10,11 +10,10 @@ var tweetObj = {
   sourceid: "Twitter API",
   userid: ''
 };
-
-//on change of $inputSocialMedia -> disable Sample text box and the other way around
-
+var userName;
 var socialMediaData = {};
 var tweetArr = [];
+
 
 $submit.click(function(evt) {
   evt.preventDefault();
@@ -24,13 +23,13 @@ $submit.click(function(evt) {
    var url = domain + inputData;
       $.get(url)
         .then(function(result) {
+          console.log(result);
           socialMediaData.statuses = result.statuses;
           var profile = JSON.stringify({'contentItems': createDataObject(socialMediaData)});
           getAnalysis(profile)
         })
         .catch(function(reason){$('p').fadeIn(1000).delay(1000).fadeOut(1000)})
   } else {
-    console.log($textInput.val());
     getTextAnalysis($textInput);
   }
 })
@@ -40,7 +39,7 @@ function createDataObject(socialMediaData) {
   var statusesArr = socialMediaData.statuses;
   statusesArr.forEach(function(status) {
     tweetObj.content = status.text,
-    tweetObj.created =
+    tweetObj.created = '',
     tweetObj.id = status.id
     tweetObj.userid = status.user['screen_name']
     tweetArr.push(tweetObj)
@@ -62,6 +61,7 @@ function getAnalysis(profile) {
       var personalityData = analysis.personality;
       personalityData.forEach(category => analysisData.push(Math.round(category.percentile * 100)))
       createChart(analysisData);
+      socialMediaData = {};
     })
 }
 
@@ -69,7 +69,7 @@ function getTextAnalysis(textInput) {
   $.ajax({
     type: 'POST',
     url: 'https://galvanize-cors-proxy.herokuapp.com/https://watson-api-explorer.mybluemix.net/personality-insights/api/v3/profile?version=2017-02-01',
-    contentType: 'text',
+    contentType: 'text/plain',
     data: textInput,
     headers: {
       "Authorization": "Basic Y2RjZjZjMWEtN2Q0Yy00YzA0LWJiNmUtZTE4MmIxNDdmMzAwOkdna1dBUGNQREZhMQ=="
@@ -84,67 +84,41 @@ function getTextAnalysis(textInput) {
 //map analysis object returned by getAnalysis function to a chart
 var analysisData = [];
 function createChart(analysisData) {
-var $ctx = $('#personalityChart')
-var bigFiveChart = new Chart($ctx, {
-  type: 'polarArea',
-  data: {
-    labels: [
-      "Openness",
-      "Conscientiousness",
-      "Extraversion",
-      "Agreeableness",
-      "Neuroticism"
-    ],
-    datasets: [{
-      data: analysisData,
-      label: userName,
-      backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-        'rgba(255, 159, 64, 0.2)'
-              ],
-      borderColor: [
-        'rgba(255,99,132,1)',
-        'rgba(54, 162, 235, 1)',
-        'rgba(255, 206, 86, 1)',
-        'rgba(75, 192, 192, 1)',
-        'rgba(153, 102, 255, 1)',
-        'rgba(255, 159, 64, 1)'
-              ],
-      borderWidth: 1
-      // hoverBackgroundColor: [],
-    }]
-  }
+  console.log(analysisData);
+  var $ctx = $('#personalityChart')
+  var bigFiveChart = new Chart($ctx, {
+    type: 'polarArea',
+    data: {
+      labels: [
+        "Openness",
+        "Conscientiousness",
+        "Extraversion",
+        "Agreeableness",
+        "Neuroticism"
+      ],
+      datasets: [{
+        data: analysisData,
+        label: userName || 'sample',
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.7)',
+          'rgba(54, 162, 235, 0.7)',
+          'rgba(255, 206, 86, 0.7)',
+          'rgba(75, 192, 192, 0.7)',
+          'rgba(153, 102, 255, 0.7)',
+          'rgba(255, 159, 64, 0.7)'
+                ],
+        borderColor: [
+          'rgba(255,99,132,1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'
+                ],
+        borderWidth: 1
+        // hoverBackgroundColor: [],
+      }]
+    }
 })
 
 }
-
-
-//slider animation
-$('.carousel').carousel();
-
-// var $sliderList = $('.slider').find('.slides');
-// var $slides = $sliderList.find('.slide');
-// var currentSlide = 1;
-// $('#first-img').fadeIn(2000);
-//
-// //create a function to animate the ul of slides, changing margin dynamically of each slide element over 3 seconds
-// function animateSlides(){
-//   $sliderList.animate({
-//     'margin-left':  -100 * (currentSlide-1) + "%"}, 5000);
-// }
-//
-// var carouselInt = "";
-// var carousel = function() {
-//   carouselInt = setInterval(function() {
-//     if(currentSlide === $slides.length) {
-//       currentSlide = 1;
-//     } else {currentSlide = currentSlide + 1}
-//     animateSlides();
-//
-//   }, 3000);
-// };
-// carousel();
